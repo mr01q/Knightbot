5111659b0b2ef11822d0547ccf32284c37fd8672
mr01q <freefiregunda77@gmail.com>
2024-10-27 18:26:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "MR DARKNESS Bot",
+  botOwner: 'MR DARKNESS', // Your name
+  ownerNumber: '917439353662', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
